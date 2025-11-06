1ed2d95a0d1658175c94ee4a3550b24f25ca99f8
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2025-11-06 18:37:36 -0300

    Ahora si arregle

--- BACKEND/models/sala_model.py
+++ BACKEND/models/sala_model.py
@@ -23,26 +23,3 @@ class SalaResponse(BaseModel):
 	nombre: str
 	tipo: str
 	capacidad: int
-
-# sanciones
-class SancionCreate(BaseModel):
-    id_participante: int
-    fecha_inicio: date
-    fecha_fin: date
-    motivo: Optional[str] = None
-
-
-class SancionUpdate(BaseModel):
-    id_participante: Optional[int] = None
-    fecha_inicio: Optional[date] = None
-    fecha_fin: Optional[date] = None
-    motivo: Optional[str] = None
-
-
-class SancionResponse(BaseModel):
-    id_sancion: int
-    id_participante: int
-    fecha_inicio: date
-    fecha_fin: date
-    motivo: Optional[str] = None
-    created_at: datetime
